58ea4460300222f2d763e43d52a8f7d2247a01f8
Jeremy Fox <jeremy.daniel.fox@gmail.com>
2015-01-31 15:14:55 -0500

    Create segregation xml file and update Segregation model

--- src/Segregation.java
+++ src/Segregation.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 import javafx.scene.paint.Color;
 
+/**
+ * 
+ * This is the Segregation model based off of Thomas Schelling's model
+ * 
+ * @author Jeremy, Team 12
+ *
+ */
 public class Segregation extends BaseModel {
 	public static final int EMPTY = 0;
 	public static final int GROUP_ONE = 1;
@@ -17,7 +24,7 @@ public class Segregation extends BaseModel {
 	public static final Color DEFAULT_COLOR = EMPTY_COLOR;
 	public static final float RATIO_NEIGHBORS = (float) .5;
 
-	public Segregation(Map<String, Double> parameters, int points) {
+	public Segregation(Map<String, Double> parameters) {
 		// possibly we should create a setNumPointsForNeighbor method in the
 		// superclass so that we can
 		// set up that information using the constructor
@@ -35,10 +42,36 @@ public class Segregation extends BaseModel {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public void updateAllCells(BaseGraph myGraph) {
 		List<Cell> myCells = getRandomCollection(myGraph);
-		
+		Cell myUnhappyCell = null;
+		Cell myEmptyCell = null;
+		for (Cell current : myCells) {
+			if (isReadyToSwitch(myUnhappyCell, myEmptyCell, myGraph))
+				break;
+			if (!isHappy(current, myGraph.getNeighbors(current)))
+				myUnhappyCell = current;
+			if (current.getCurrentState() == EMPTY)
+				myEmptyCell = current;
+		}
+		if (isReadyToSwitch(myUnhappyCell, myEmptyCell, myGraph)) {
+			myUnhappyCell.setFutureState(myEmptyCell.getCurrentState());
+			myUnhappyCell.getShape().setFill(EMPTY_COLOR);
+			myEmptyCell.setFutureState(myUnhappyCell.getCurrentState());
+			if (myUnhappyCell.getCurrentState()==GROUP_ONE)
+				myEmptyCell.getShape().setFill(GROUP_TWO_COLOR);
+			else
+				myEmptyCell.getShape().setFill(GROUP_ONE_COLOR);
+		}
+
+	}
+
+	private boolean isReadyToSwitch(Cell first, Cell second, BaseGraph myGraph) {
+		return !isHappy(first, myGraph.getNeighbors(first))
+				&& second.getCurrentState() == EMPTY
+				|| !isHappy(second, myGraph.getNeighbors(second))
+				&& first.getCurrentState() == EMPTY;
 	}
 
 	@Override
@@ -52,8 +85,8 @@ public class Segregation extends BaseModel {
 	}
 
 	private boolean isHappy(Cell myCell, Collection<Cell> myNeighbors) {
-		float myState = myCell.getCurrentState();
-		if (myState==EMPTY)	
+		int myState = myCell.getCurrentState();
+		if (myState == EMPTY)
 			return true;
 		float countDifferent = 0;
 		for (Cell current : myNeighbors) {
@@ -64,7 +97,7 @@ public class Segregation extends BaseModel {
 		float ratio = countDifferent / (float) myNeighbors.size();
 		return ratio <= RATIO_NEIGHBORS;
 	}
-	
+
 	private List<Cell> getRandomCollection(BaseGraph myGraph) {
 		List<Cell> myCells = (List<Cell>) myGraph.getAllCells();
 		Collections.shuffle(myCells);
