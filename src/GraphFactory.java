f5b878faa383aad6cd6400eef718cfbefc4b4bc7
Jeremy Fox <jeremy.daniel.fox@gmail.com>
2015-02-04 23:31:30 -0500

    add EightNeighborSquareGraph

--- src/GraphFactory.java
+++ src/GraphFactory.java
@@ -1,4 +1,3 @@
-
 import javafx.scene.paint.Color;
 
 /**
@@ -8,20 +7,33 @@ import javafx.scene.paint.Color;
  *
  */
 public class GraphFactory {
+	private String myType;
+	
+	public GraphFactory() {
+		
+	}
+
+	public GraphFactory(String type) {
+		myType = type;
+	}
+
 	public BaseGraph createSpecifiedGraph(int numCellsWidth,
 			int numCellsHeight, int screenWidth, int screenHeight, int xOffset,
 			int yOffset, int points, int defaultState, Color defaultColor,
 			String model) {
 
 		switch (model) {
+		case "EightNeighor":
+			return new EightNeighborSquareGraph(numCellsWidth,numCellsHeight,screenWidth,screenHeight,
+					xOffset,yOffset,points,defaultState,defaultColor,model);
 //		case "WaTorWorld":
 //			return new ToroidSquareGraph(numCellsWidth, numCellsHeight,
 //					screenWidth, screenHeight, xOffset, yOffset, points,
 //					defaultState, defaultColor, model);
-//		case "GameOfLife":
-//			return new ToroidSquareGraph(numCellsWidth, numCellsHeight,
-//					screenWidth, screenHeight, xOffset, yOffset, points,
-//					defaultState, defaultColor, model);
+		case "GameOfLife":
+			return new EightNeighborSquareGraph(numCellsWidth, numCellsHeight,
+					screenWidth, screenHeight, xOffset, yOffset, points,
+					defaultState, defaultColor, model);
 		default:
 			return new SquareGraph(numCellsWidth, numCellsHeight, screenWidth,
 					screenHeight, xOffset, yOffset, points, defaultState,
