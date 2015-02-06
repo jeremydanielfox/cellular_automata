82ca83dc2db1cf7cae489d92638de8878ef3209d
Sierra Smith <sms95@duke.edu>
2015-02-06 15:47:00 -0500

    Eliminated commented out test case in Brain

--- src/SimBrain.java
+++ src/SimBrain.java
@@ -220,18 +220,6 @@ public class SimBrain extends Application {
 			myIncSpeedButton.setDisable(false);
 			myDecSpeedButton.setDisable(false);
 			enableCorrectButtons(false);
-
-			// added for testing
-//			Map<String, ArrayList<Double>> paramMap = new HashMap<>();
-//			ArrayList<Double> toAdd = new ArrayList<Double>();
-//			toAdd.add(0, (double) 0);
-//			toAdd.add(1, (double) 5);
-//			toAdd.add(2, 2.5);
-//			paramMap.put("probCatch", toAdd);
-//			toAdd.add(0, (double) 0);
-//			toAdd.add(1, (double) 5);
-//			toAdd.add(2, 2.5);
-//			paramMap.put("second", toAdd);
 			
 			Map<String, ArrayList<Double>> paramMap = myEngine.getParamMap();
 			
