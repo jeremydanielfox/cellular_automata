dcc8e7d5c173137aa392381f6dc9d98129fbcc33
Jeremy Fox <jeremy.daniel.fox@gmail.com>
2015-02-08 22:24:21 -0500

    Remove unnecessary package imports

--- src/CellsAndComponents/Patch.java
+++ src/CellsAndComponents/Patch.java
@@ -1,14 +1,14 @@
 package CellsAndComponents;
+
 import javafx.scene.paint.Color;
 
-
 public abstract class Patch {
-	
+
 	public Patch() {
 	}
-	
+
 	public abstract int getStateForCell();
-	
+
 	public abstract Color getColor();
-	
+
 }
--- src/CellsAndComponents/Sugar.java
+++ src/CellsAndComponents/Sugar.java
@@ -1,11 +1,11 @@
 package CellsAndComponents;
-import javafx.scene.paint.Color;
 
+import javafx.scene.paint.Color;
 
 public class Sugar extends Patch {
 	private int currentAmount;
 	private int maxAmount;
-	
+
 	public Sugar(int max) {
 		maxAmount = max;
 		currentAmount = maxAmount;
@@ -20,28 +20,25 @@ public class Sugar extends Patch {
 	public Color getColor() {
 		return Color.ORANGE;
 	}
-	
+
 	public int getSugarAmount() {
 		return currentAmount;
 	}
-	
+
 	public void takeAllSugar() {
 		currentAmount = 0;
 	}
-	
+
 	public void takeSugar(int sugarToTake) {
 		currentAmount -= sugarToTake;
 	}
-	
+
 	public void sugarGrowBack(int sugarGrowBackRate) {
-		if (currentAmount + sugarGrowBackRate >= maxAmount){
+		if (currentAmount + sugarGrowBackRate >= maxAmount) {
 			currentAmount = maxAmount;
-		}
-		else {
+		} else {
 			currentAmount += sugarGrowBackRate;
 		}
 	}
-	
-	
 
 }
